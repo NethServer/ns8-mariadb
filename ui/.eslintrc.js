757021571c64ff8794041232daf3faa909ab8290
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-12-04 14:54:43 +0100

    chore(eslint): update parser from babel-eslint to eslint-parser

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "eslint-parser",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
